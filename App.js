18402bd9121a73177cae5017f8807779b79bd1c1
profpatriciabassan <patricia.bassan@unisantacruz.edu.br>
2025-09-01 20:28:17 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,15 @@
-ECHO est  ativado.
+import { ScrollView, Text } from 'react-native';
+
+export default function App() {
+  return (
+    <ScrollView style={{ padding: 16 }}>
+      {Array.from({ length: 9 }, (_, i) => i + 1).map((n) => (
+        <Text key={n} style={{ marginBottom: 16, fontSize: 16 }}>
+          {`Tabuada do ${n}\n` +
+            Array.from({ length: 10 }, (_, j) => `${n} x ${j + 1} = ${n * (j + 1)}`).join('\n')}
+        </Text>
+      ))}
+    </ScrollView>
+  );
+}
+
